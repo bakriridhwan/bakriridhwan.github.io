cd1b0a4c96535e3ff7fd5022e4f4599f9363dee7
bakriahmadridhwan <bahridhwan@gmail.com>
2020-07-20 23:07:56 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// PARALLAX
+// PARAGRAF ABOUT
+$(window).on('load', function () {
+    $('.bakri-pkiri').addClass('pmuncul');
+    $('.bakri-pkanan').addClass('pmuncul');
+});
+
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform': `translate(0px, ${wScroll / 4}%)`
+    });
+
+    $('.jumbotron h1').css({
+        'transform': `translate(0px, ${wScroll / 2}%)`
+    });
+
+    $('.jumbotron p').css({
+        'transform': `translate(0px, ${wScroll / 1.2}%)`
+    });
+
+
+    // PORTOFOLIO
+    if (wScroll > $('.portofolio').offset().top - 250) {
+        $('.portofolio .thumbnail').each(function (i) {
+            setTimeout(function () {
+                $('.portofolio .thumbnail').eq(i).addClass('muncul');
+            }, 300 * (i + 1));
+        });
+
+    }
+
+
+});
+
+
+
+// ABOUT
+$(window).on('load', function () {
+    $('.bakri-pkiri').addClass('pmuncul');
+});
